0f21447315efd6ef6c033f143ca65a39e9acb94e
Rafael Sánchez Córdoba <53056368+RafaelSanchezCordoba@users.noreply.github.com>
2024-12-20 16:55:38 +0100

    Revert "Feature/project init"

--- src/app/(rs)/customers/page.tsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: "Customers",
-};
-
-export default function Customers() {
-  return <h2>Customers Page</h2>;
-}
--- src/app/(rs)/tickets/page.tsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: "Tickets",
-};
-
-export default function Tickets() {
-  return <h2>Tickets Page</h2>;
-}
